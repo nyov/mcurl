50972e8856f4d1bd890c436bb1266f797d01c270
nyov <nyov@nexnode.net>
2014-03-12 03:40:26 +0000

    indentation fixes

--- MementoParser.pm
+++ MementoParser.pm
@@ -6,30 +6,22 @@ use LWP::Simple ();
 
 our @localURIs;
 #sub new {
-
-
-#    my $self = {
-#
-#        embeddedURIs => undef
-#    };
-#   bless $self, 'MementoParser';
-#    return $self;
+#	my $self = {
+#		embeddedURIs => undef
+#	};
+#	bless $self, 'MementoParser';
+#	return $self;
 #}
 sub start {
-
 	my ($self, $tagname, $attr) = @_;
 
-		if ($tagname eq 'img') {
+	if ($tagname eq 'img') {
 		my $url = $attr->{ src };
 		#print "img found: $url\n";
 		if($url)
 		{
 			push(@localURIs,$url);
-
-
 		}
-
-
 	} elsif($tagname eq 'script') {
 		my $url = $attr->{ src };
 		if($url)
@@ -37,13 +29,12 @@ sub start {
 			push(@localURIs,$url);
 		}
 	}
-		#print "\n==========================================\n";
-		#print @localURIs;
-
+	#print "\n==========================================\n";
+	#print @localURIs;
 }
 
 sub returnURIs {
-		my ($self) = @_;
+	my ($self) = @_;
 	#print "in returnURIs\n @embeddedURIs \n";
 	return(@localURIs );
 }

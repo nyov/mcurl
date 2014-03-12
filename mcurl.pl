6ede4d988a343aeef01d986bf387e0892d60d471
nyov <nyov@nexnode.net>
2014-03-12 02:32:47 +0000

    whitespace changes
    crlf > lf
    spaces > tabs

--- mcurl.pl
+++ mcurl.pl
@@ -20,7 +20,7 @@ my $mode = '';
 my $debug = 0;
 my $override = 0;
 my $replacefile = '';
-for (my $i = 0; $i <= $#ARGV; ++$i)	# 
+for (my $i = 0; $i <= $#ARGV; ++$i)	#
 {
 	if($ARGV[$i] eq '--help')
 	{
@@ -28,8 +28,8 @@ for (my $i = 0; $i <= $#ARGV; ++$i)	#
 		$command =~ s/curl/mcurl/g;
 		my $mcurlHelp  = " -tm/--timemap <link|rdf> To select the type of Timemap it may be link or html\n";
 		$mcurlHelp .= " -tg/--timegate <uri[,uri]> To select the favorite Timegates\n";
-		$mcurlHelp .= " -dt/--datetime <date in rfc822 format> Select the date in the past (For example, Thu, 31 May 2007 20:35:00 GMT)\n"; 
-                $mcurlHelp .= " -mode  <strict|relaxed> Specify mcurl embedded resource policy, default value is relaxed\n";
+		$mcurlHelp .= " -dt/--datetime <date in rfc822 format> Select the date in the past (For example, Thu, 31 May 2007 20:35:00 GMT)\n";
+				$mcurlHelp .= " -mode  <strict|relaxed> Specify mcurl embedded resource policy, default value is relaxed\n";
 		print $command.$mcurlHelp;
 		exit;
 	}elsif($ARGV[$i] eq '--timegate' or $ARGV[$i] eq '-tg')
@@ -37,26 +37,26 @@ for (my $i = 0; $i <= $#ARGV; ++$i)	#
 		$ARGV[$i] = '';
 		$timegate = $ARGV[++$i];
 		$ARGV[$i] = '';
-		
-		
+
+
 	}elsif($ARGV[$i] eq '--override' )
 	{
 		$ARGV[$i] = '';
 		$override = 1;
 		#$ARGV[$i] = '';
-		
-		
+
+
 	}elsif($ARGV[$i] eq '--timemap' or $ARGV[$i] eq '-tm')
 	{
-                # we need to fix this line to add the default timemap
+				# we need to fix this line to add the default timemap
 		$ARGV[$i] = '';
-                if (index($ARGV[$i+1],'-') == 0){
-                    $timemap =  'link';    
-                } else {
-                    $timemap =  $ARGV[++$i];
-                }
+				if (index($ARGV[$i+1],'-') == 0){
+					$timemap =  'link';
+				} else {
+					$timemap =  $ARGV[++$i];
+				}
 		$ARGV[$i] = '';
-	
+
 	}elsif($ARGV[$i] eq '--datetime' or $ARGV[$i] eq '-dt')
 	{
 		$ARGV[$i] = '';
@@ -65,55 +65,55 @@ for (my $i = 0; $i <= $#ARGV; ++$i)	#
 
 	}elsif($ARGV[$i] eq '--dateTimeRange')
 	{
-	
+
 	}elsif($ARGV[$i] eq '--replacedump')
 	{
 		$ARGV[$i] = '';
 		$replacefile =$ARGV[++$i];
 		$ARGV[$i] = '';
 	}elsif($ARGV[$i] eq '--mode')
-        {
-            $ARGV[$i] = '';
-            $mode = $ARGV[++$i];
-            $ARGV[$i] = '';
-        }elsif($ARGV[$i] eq '--debug')
-        {
-            $ARGV[$i] = '';
-            $debug = 1;
+		{
+			$ARGV[$i] = '';
+			$mode = $ARGV[++$i];
+			$ARGV[$i] = '';
+		}elsif($ARGV[$i] eq '--debug')
+		{
+			$ARGV[$i] = '';
+			$debug = 1;
 
-        }elsif($ARGV[$i] eq '--version' or $ARGV[$i] eq '-V' ){
-            $ARGV[$i] = '';
-            my $mcurlVer = `curl -V`;
-            #$mcurlVer =~ s/curl [\S]*/mcurl 0.1 Memento Enabled curl/;
-            print "mcurl 0.86 Memento Enabled curl based on " .$mcurlVer;
-            exit;
-        }
+		}elsif($ARGV[$i] eq '--version' or $ARGV[$i] eq '-V' ){
+			$ARGV[$i] = '';
+			my $mcurlVer = `curl -V`;
+			#$mcurlVer =~ s/curl [\S]*/mcurl 0.1 Memento Enabled curl/;
+			print "mcurl 0.86 Memento Enabled curl based on " .$mcurlVer;
+			exit;
+		}
 }
 
 my $URI = $ARGV[$#ARGV];
 $ARGV[$#ARGV] = '';
 
-for (my $i = 0; $i <= $#ARGV; ++$i)	# 
+for (my $i = 0; $i <= $#ARGV; ++$i)	#
 {
-    if ( index($ARGV[$i] , ' ') > -1 ){
+	if ( index($ARGV[$i] , ' ') > -1 ){
 $ARGV[$i] = '"' .$ARGV[$i] . '"';
-    }
+	}
 }
 my $mt = new MementoThread();
 
 #Fill the parameters
 if(length($timegate) != 0){
-    $mt->setTimeGate($timegate);
+	$mt->setTimeGate($timegate);
 }
 
 if($mode eq 'strict'){
-    $mt->setMode(1);
+	$mt->setMode(1);
 }
 
 
 $mt->setURI($URI);
 $mt->setDateTime($acceptDateTimeHeader);
-            
+
 $mt->setDebug($debug);
 $mt->setOverride($override);
 $mt->setReplaceFile($replacefile);
@@ -124,20 +124,20 @@ $mt->head();
 $mt->handle_redirection();
 
 if( $timemap )
-{	
-     my $results= $mt->process_timemap( $timemap ,@ARGV );
+{
+	 my $results= $mt->process_timemap( $timemap ,@ARGV );
 
 
-    print "\n--------------------------THE PAGE CONTENT-------------------------------------------\n";
-    print $results;
-    print "\n--------------------------END PAGE CONTENT-------------------------------------------\n";
+	print "\n--------------------------THE PAGE CONTENT-------------------------------------------\n";
+	print $results;
+	print "\n--------------------------END PAGE CONTENT-------------------------------------------\n";
 
 } else {
-    
-    my $results= $mt->process_uri(@ARGV );
-    print "\n--------------------------THE PAGE CONTENT-------------------------------------------\n";
-    print $results;
-    print "\n--------------------------END PAGE CONTENT-------------------------------------------\n";
+
+	my $results= $mt->process_uri(@ARGV );
+	print "\n--------------------------THE PAGE CONTENT-------------------------------------------\n";
+	print $results;
+	print "\n--------------------------END PAGE CONTENT-------------------------------------------\n";
 }
 #'Thu, 31 May 2007 20:35:00 GMT'
 
--- mementoParser.pm
+++ mementoParser.pm
@@ -16,20 +16,20 @@ our @localURIs;
 #    return $self;
 #}
 sub start {
-        
+
 	my ($self, $tagname, $attr) = @_;
-       
-        if ($tagname eq 'img') {
+
+		if ($tagname eq 'img') {
 		my $url = $attr->{ src };
 		#print "img found: $url\n";
 		if($url)
 		{
 			push(@localURIs,$url);
 
-			
+
 		}
-	
-		
+
+
 	} elsif($tagname eq 'script') {
 		my $url = $attr->{ src };
 		if($url)
@@ -37,13 +37,13 @@ sub start {
 			push(@localURIs,$url);
 		}
 	}
-        #print "\n==========================================\n";
-        #print @localURIs;
-       
+		#print "\n==========================================\n";
+		#print @localURIs;
+
 }
 
 sub returnURIs {
-        my ($self) = @_;
+		my ($self) = @_;
 	#print "in returnURIs\n @embeddedURIs \n";
 	return(@localURIs );
 }
